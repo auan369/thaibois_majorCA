5eb49fe2da3645a6aa3d152054ee2fd0646f805d
lohxinzhi <lxzraizer@gmail.com>
2024-04-04 23:37:27 +0800

    create dev branch and split program to different processes

---
+++ main.c
@@ -0,0 +1,17 @@
+// Program to manage and run all program
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <process.h>
+// #include <sys/types.h>
+// #include <sys/neutrino.h>
+// #include <unistd.h>
+// #include <sched.h>
+
+
+int main(int argc, char const *argv[]){
+    
+    // TODO
+    system("");
+    return 0;
+}
---
+++ user_input.c
@@ -0,0 +1,9 @@
+// this program is to constant ask for user input using keyboard the change settings 
+// for generating the wave
+
+// TODO
+
+int main(int argc, char const *argv[]){
+
+    exit();
+}
