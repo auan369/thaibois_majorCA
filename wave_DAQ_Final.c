2a29eebee7890e003dbab6d4565a939ea522b8a9
toastedbaguatte <155293525+toastedbaguatte@users.noreply.github.com>
2024-04-25 17:25:08 +0800

    Update wave_DAQ_Final.c

--- wave_DAQ_Final.c
+++ wave_DAQ_Final.c
@@ -90,7 +90,7 @@ int parse_arg(int argc, char const *argv[], int *wave, int *freq, char file[], c
 			else {
 				printf("invalid input. Loading Default Sine waveform\n");
 				*wave=SINE_WAVE;
-				} 
+				}
 		}
 		
 		else if (strcmp(argv[i], "-f")==0){ //frequency flag
@@ -130,9 +130,7 @@ return 0; // if everything is parse correctlly
 //--------------------------------------------------------------read and write file-------------------------------------------------------------
 
 void readFile(char filename[]){
-
 	FILE *file = fopen(filename, "r"); // can add missing file check here
-
 	if(file != NULL){
 		printf("\nRead file %s\n", filename);
 		fscanf(file, "%d", &waveform);
@@ -164,8 +162,6 @@ void writeFile(char filename[]){
 
 		fclose(file); }
 }
-
-
 
 void print_wave_settings(){
 	   switch  (waveform){
@@ -262,39 +258,18 @@ float genWave(int index, int waveform){
   }
 
 }
+
 //------------------------------------------- Define Threads ------------------------------------------
-void* function1( void* arg )
-{
-  while( 1 ) {
-    pthread_mutex_lock( &mutex );
-    // TODO : RECEIVE MESSAGE FROM input.c FOR USER SETTINGS
-    if (i<steps){
-      data = genWave(i++, waveform);
-    }
-    else{
-      i = 0;
-      data = genWave(i++, waveform);
-    }
-    pthread_mutex_unlock( &mutex );
-    usleep(1000000/frequency); //sleep for 1ms, allows next thread to run
-  }
-
-}
-
 void* audio_thread( void* arg )
 {
   while( 1 ) {
-  	//printf("Audio thread\n");
-    //pthread_mutex_lock( &mutex );
+    pthread_mutex_lock(&mutex);
     printf("\a"); // Auditory cue
-
-    //pthread_mutex_unlock( &mutex );
+    pthread_mutex_unlock(&mutex);
     usleep( 1000000/frequency); // Wait for the beat rate//sleep for 1ms, allows next thread to run
   }
 }
-//
-------------------------------------------------------------input thread ------------------------------------------------------------
-// thread to take in input from terminal
+
 void *input_thread(void *arg) {
     char input[10];
     char c;
@@ -302,68 +277,61 @@ void *input_thread(void *arg) {
     
     while (1) {
   	   if (scanf("%s", input)==1){
-       	if (sscanf(input, "%d", &num) ==1) {
-       		pthread_mutex_lock( &mutex );
-       		printf("\nInput Frequency: %d\n", num);
-       		if (num > 0){ //error check for frequency
-       			frequency=num;
-       		}
-       		else{
-       			printf("Invalid Input Frequency. Please try again\n");
-       		}
-       		writeFile(out_filename);
-       		print_wave_settings();
-       		pthread_mutex_unlock( &mutex );
-       	}
-       	else{
-       		if (sscanf(input, "%c", &c) ==1) {
-       			pthread_mutex_lock( &mutex );
-       			printf("Input Char is: %c\n", c);
-       			 switch (c) {
-            		case '+':
-              	  		frequency += 1;
-              	  		//writeFile();
-                		break;
-            		case '-':
-                		if (frequency > 1) {
-                    		frequency-=1;
-                    		//writeFile();
-                		}
-                		break;
-                	case 'S':
-                	case 's':
-                		waveform = SINE_WAVE;
-                		//writeFile();
-                		break;
-                	case 'W':
-                	case 'w':
-                		waveform = SAWTOOTH_WAVE;
-                		//writeFile();
-                		break;
-                	case 'Q':
-                	case 'q':
-                		waveform = SQUARE_WAVE;
-                		//writeFile();
-                		break;
-              	 	case 'T':
-					case 't':
-                		waveform = TRIANGLE_WAVE;
-                		//writeFile();
-                		break;
-                	case 'E':
-					case 'e':
-                		waveform = SINESAWTOOTH_WAVE;
-                		//writeFile();
-                		break;
-            		default: //error check for waveform
-            			printf("Invalid char input. Please put only 'S or s', 'W or w', 'Q or q', 'T or t', 'E or e' as inputs\n");
-                		break;
-        		}
-        		writeFile(out_filename);
-        		print_wave_settings();
-        		pthread_mutex_unlock(&mutex);
-       		}
-       	}
+			if (sscanf(input, "%d", &num) ==1) {
+				pthread_mutex_lock( &mutex );
+				printf("\nInput Frequency: %d\n", num);
+				if (num > 0){ //error check for frequency
+					frequency=num;
+				}
+				else{
+					printf("Invalid Input Frequency. Please try again\n");
+				}
+				writeFile(out_filename);
+				print_wave_settings();
+				pthread_mutex_unlock( &mutex );
+			}
+			else{
+				if (sscanf(input, "%c", &c) ==1) {
+					pthread_mutex_lock( &mutex );
+					printf("Input Char is: %c\n", c);
+					switch (c) {
+						case '+':
+							frequency += 1;
+							break;
+						case '-':
+							if (frequency > 1) {
+								frequency-=1;
+							}
+							break;
+						case 'S':
+						case 's':
+							waveform = SINE_WAVE;
+							break;
+						case 'W':
+						case 'w':
+							waveform = SAWTOOTH_WAVE;
+							break;
+						case 'Q':
+						case 'q':
+							waveform = SQUARE_WAVE;
+							break;
+						case 'T':
+						case 't':
+							waveform = TRIANGLE_WAVE;
+							break;
+						case 'E':
+						case 'e':
+							waveform = SINESAWTOOTH_WAVE;
+							break;
+						default: //error check for waveform
+							printf("Invalid char input. Please put only 'S or s', 'W or w', 'Q or q', 'T or t', 'E or e' as inputs\n");
+							break;
+					}
+					writeFile(out_filename);
+					print_wave_settings();
+					pthread_mutex_unlock(&mutex);
+				}
+			}
        }
       usleep( 1000000/frequency); //sleep for 1ms, allows next thread to run
     }
@@ -387,107 +355,97 @@ void *visual_thread(void *arg) {
     return NULL;
 }
 
-
 //!--------------------------------------------------Main Function--------------------------------------------------!
 int main(int argc, char const *argv[]){
 
-//----------------------declaration----------------------
-struct pci_dev_info info;
-void *hdl;
-uintptr_t iobase[6];
-uintptr_t dio_in;
-uint16_t adc_in;
-unsigned int i,j,count;
-unsigned short chan;
-unsigned int data1[5][50];
-float delta,dummy;
+	//----------------------declaration----------------------
+	struct pci_dev_info info;
+	void *hdl;
+	uintptr_t iobase[6];
+	uintptr_t dio_in;
+	uint16_t adc_in;
+	unsigned int i,j,count;
+	unsigned short chan;
+	unsigned int data1[5][50];
+	float delta,dummy;
 
-printf("\fDemonstration Routine for PCI-DAS 1602\n\n");
+	printf("\fDemonstration Routine for PCI-DAS 1602\n\n");
 
-memset(&info,0,sizeof(info));
-if(pci_attach(0)<0) {
-  perror("pci_attach");
-  exit(EXIT_FAILURE);
-  }
-
-/* Vendor and Device ID */
-info.VendorId=0x1307;
-info.DeviceId=0x01;																		
-
-
-
-if ((hdl=pci_attach_device(0, PCI_SHARE|PCI_INIT_ALL, 0, &info))==0) {
-  perror("pci_attach_device");
-  exit(EXIT_FAILURE);
-  }
-  																					
-// Determine assigned BADRn IO addresses for PCI-DAS1602
-printf("\nDAS 1602 Base addresses:\n\n");
-for(i=0;i<5;i++) {
-  badr[i]=PCI_IO_ADDR(info.CpuBaseAddress[i]);
-  printf("Badr[%d] : %x\n", i, badr[i]);
-  }
-
-// map I/O base address to user space
-printf("\nReconfirm Iobase:\n");  												
-for(i=0;i<5;i++) {												//expect CpuBaseAddress to be the same as iobase for PC
-  iobase[i]=mmap_device_io(0x0f,badr[i]);	
-  printf("Index %d : Address : %x ", i,badr[i]);
-  printf("IOBASE  : %x \n",iobase[i]);
-  }
-
-// Modify thread control privity																		
-if(ThreadCtl(_NTO_TCTL_IO,0)==-1) {
-  perror("Thread Control");
-  exit(1);
-  }
-printf("\n");
-
-
-//-------------------------------------------------------------parse arg------------------------------------------------------
-if (parse_arg(argc, argv, &waveform, &frequency, filename, out_filename)) exit(1);
-
- // exit program if parse arg fails
-if (!outfile_exist) printf("No output file given. Saving settings to default output file new_settings.txt\n" );
-
-// generate the data points for the 4 waveforms
-for (j=0;j<5;j++){
-	for(i=0;i<50;i++) {
-  	dummy = genWave(i, j)* 0x8000;
- 	data1[j][i]= (unsigned) dummy;			// add offset +  scale
- 	//printf("%f\n", dummy);
+	memset(&info,0,sizeof(info));
+	if(pci_attach(0)<0) {
+		perror("pci_attach");
+		exit(EXIT_FAILURE);
 	}
-}
-  
-  
-if (file_exist) readFile(filename);
- // it will priorities file settings over command line settings
-	writeFile(out_filename);
-  	print_wave_settings();
-  	//pthread_create( &th[0], NULL, &function1, NULL );
-  	pthread_create( &th[1], NULL, &visual_thread, NULL );
-  	pthread_create( &th[2], NULL, &audio_thread, NULL );
-  	pthread_create( &th[3], NULL, &input_thread, NULL );
-  
-while(1) {
-//printf("Main thread\n");
-	for(i=0;i<50;i++) {
-		out16(DA_CTLREG,0x0a23);			// DA Enable, #0, #1, SW 5V unipolar		2/6
-		out16(DA_FIFOCLR, 0);					// Clear DA FIFO  buffer
-		out16(DA_Data,(short) data1[waveform][i]);																																		
-		out16(DA_CTLREG,0x0a43);			// DA Enable, #1, #1, SW 5V unipolar		2/6
-		out16(DA_FIFOCLR, 0);					// Clear DA FIFO  buffer
-		out16(DA_Data,(short) data1[waveform][i]);			
+
+	/* Vendor and Device ID */
+	info.VendorId=0x1307;
+	info.DeviceId=0x01;																		
+
+	if ((hdl=pci_attach_device(0, PCI_SHARE|PCI_INIT_ALL, 0, &info))==0) {
+		perror("pci_attach_device");
+		exit(EXIT_FAILURE);
+	}
+																						
+	// Determine assigned BADRn IO addresses for PCI-DAS1602
+	printf("\nDAS 1602 Base addresses:\n\n");
+	for(i=0;i<5;i++) {
+		badr[i]=PCI_IO_ADDR(info.CpuBaseAddress[i]);
+		printf("Badr[%d] : %x\n", i, badr[i]);
+	}
+
+	// map I/O base address to user space
+	printf("\nReconfirm Iobase:\n");  												
+	for(i=0;i<5;i++) {	//expect CpuBaseAddress to be the same as iobase for PC
+		iobase[i]=mmap_device_io(0x0f,badr[i]);	
+		printf("Index %d : Address : %x ", i,badr[i]);
+		printf("IOBASE  : %x \n",iobase[i]);
+	}
+
+	// Modify thread control privity																		
+	if(ThreadCtl(_NTO_TCTL_IO,0)==-1) {
+		perror("Thread Control");
+		exit(1);
+	}
+	printf("\n")
+
+	//-------------------------------------------------------------parse arg------------------------------------------------------
+	if (parse_arg(argc, argv, &waveform, &frequency, filename, out_filename)) exit(1);
+
+	// exit program if parse arg fails
+	if (!outfile_exist) printf("No output file given. Saving settings to default output file new_settings.txt\n" );
+
+	// generate the data points for the 4 waveforms
+	for (j=0;j<5;j++){
+		for(i=0;i<50;i++) {
+		dummy = genWave(i, j)* 0x8000;
+		data1[j][i]= (unsigned) dummy;			// add offset +  scale
+		//printf("%f\n", dummy);
+		}
+	}
 	
-	usleep( 1000000/(frequency*50)); // Wait for time of T/steps, allows next thread to run
-  }
-}
+	if (file_exist) readFile(filename);
+	// it will priorities file settings over command line settings
+		writeFile(out_filename);
+		print_wave_settings();
+		//pthread_create( &th[0], NULL, &function1, NULL );
+		pthread_create( &th[1], NULL, &visual_thread, NULL );
+		pthread_create( &th[2], NULL, &audio_thread, NULL );
+		pthread_create( &th[3], NULL, &input_thread, NULL );
 	
-//pthread_join(th[0], NULL); // wait for the thread 0 to exit first (never happens)
-//pthread_join(th[1], NULL); // wait for the thread 1 to exit first (never happens)
-//pthread_join(th[2], NULL); // wait for the thread 2 to exit first (never happens)
-//pthread_join(th[3], NULL); // wait for the thread 3 to exit first (never happens)
-  return 0;
+	while(1) {
+	//printf("Main thread\n");
+		for(i=0;i<50;i++) {
+			out16(DA_CTLREG,0x0a23);			// DA Enable, #0, #1, SW 5V unipolar		2/6
+			out16(DA_FIFOCLR, 0);					// Clear DA FIFO  buffer
+			out16(DA_Data,(short) data1[waveform][i]);																																		
+			out16(DA_CTLREG,0x0a43);			// DA Enable, #1, #1, SW 5V unipolar		2/6
+			out16(DA_FIFOCLR, 0);					// Clear DA FIFO  buffer
+			out16(DA_Data,(short) data1[waveform][i]);			
+		usleep( 1000000/(frequency*50)); // Wait for time of T/steps, allows next thread to run
+		}
+	}
+	
+	return 0;
 }
 #/** PhEDIT attribute block
 #-11:16777215
