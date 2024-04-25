e8251e4421527fc4f7c26845d3b328514159489f
lohxinzhi <lxzraizer@gmail.com>
2024-04-25 18:41:37 +0800

    merge important file to main

--- user_input.c
+++
@@ -1,9 +0,0 @@
-// this program is to constant ask for user input using keyboard the change settings 
-// for generating the wave
-
-// TODO
-
-int user_input(int argc, char const *argv[]){
-
-    // exit();
-}
